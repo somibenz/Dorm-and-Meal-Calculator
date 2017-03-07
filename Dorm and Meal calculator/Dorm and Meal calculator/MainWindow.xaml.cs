160b2f38e64fdc8482233fd146eb7d00d66564ee
Shomi <somibenz@yahoo.com>
2017-03-07 14:07:40 -0500

    Add files via upload
    An application   with two  forms that allows a user to select a dormitory and a meal plan and displays the total charges

---
+++ Dorm and Meal calculator/Dorm and Meal calculator/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dorm_and_Meal_calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int dorm = 0;
+        int meal = 0;
+        int total = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+     
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (listBox.SelectedIndex == 0)
+                {
+                    dorm = 1500;
+                }
+                else if (listBox.SelectedIndex == 1)
+                {
+                    dorm = 1600;
+                }
+                else if (listBox.SelectedIndex == 2)
+                {
+                    dorm = 1800;
+                }
+                else if (listBox.SelectedIndex == 3)
+                {
+                    dorm = 2500;
+                }
+                if (listBox1.SelectedIndex == 0)
+                {
+                    meal = 600;
+                }
+                else if (listBox1.SelectedIndex == 1)
+                {
+                    meal = 1200;
+                }
+                else if (listBox1.SelectedIndex == 2)
+                {
+                    meal = 1700;
+                }
+                total = dorm * meal;
+                label.Content = total;
+                new form1().Show();
+                this.Hide();
+            }
+            catch (Exception)
+            {
+
+
+            }
+        }   
+    }
+}
---
+++ Dorm and Meal calculator/Dorm and Meal calculator/form11.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Dorm_and_Meal_calculator
+{
+    internal class form1
+    {
+        public form1()
+        {
+        }
+
+        internal void Show()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
